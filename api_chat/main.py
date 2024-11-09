8d1cdc449962e471c5e55de4b8d3aec31d2f7eda
Kir1llz <kyrill.zykin@gmail.com>
2024-11-09 17:27:01 +0300

    fix main

--- api_chat/main.py
+++ api_chat/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request
-from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from typing import List
 from fastapi.templating import Jinja2Templates
+import logging
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+app.mount("/static", StaticFiles(directory="templates/static"), name="static")
 
 class ConnectionManager:
     def __init__(self):
@@ -17,22 +21,41 @@ class ConnectionManager:
     def disconnect(self, websocket: WebSocket):
         self.active_connections.remove(websocket)
 
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
     async def broadcast(self, message: str):
         for connection in self.active_connections:
             await connection.send_text(message)
+
+    async def broadcast_binary(self, binary_data: bytes):
+        for connection in self.active_connections:
+            await connection.send_bytes(binary_data)
 
 manager = ConnectionManager()
 
 @app.get("/")
 async def get(request: Request):
-    return templates.TemplateResponse(request=request, name="index.html")
+    return templates.TemplateResponse(request=request, name="chat.html")
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await manager.connect(websocket)
     try:
+        await manager.send_personal_message("Welcome to the WebSocket server!", websocket)
         while True:
-            data = await websocket.receive_text()
-            await manager.broadcast(f"Client says: {data}")
+            try:
+                data = await websocket.receive()
+                logger.info(f"Received data: {data}")
+                if "text" in data and isinstance(data['type'], str):
+                    await manager.broadcast(f"{data['text']}")
+                elif "bytes" in data and isinstance(data['bytes'], bytes):
+                    await manager.broadcast_binary(data['bytes'])
+            except WebSocketDisconnect:
+                manager.disconnect(websocket)
+                break
+            except Exception as e:
+                logger.error(f"An error occurred: {e}")
+                await websocket.close(reason=str(e))
     except WebSocketDisconnect:
         manager.disconnect(websocket)
